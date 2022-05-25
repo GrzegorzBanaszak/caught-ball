e77b556b1bce4a58130776ddb98fa6cc3033bb4d
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-25 15:37:40 +0200

    Create ball style

--- src/components/Ball.tsx
+++ src/components/Ball.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-
-interface IBall {
-    children: React.ReactNode
+import React from "react";
+import styled from "styled-components";
+import "../styles/ball.css";
+interface IBallProps {
+  children: React.ReactNode;
 }
 
-const Ball = ({children}:IBall):JSX.Element => {
-  return (
-    <div>Ball</div>
-  )
+interface IBallElementProps {
+  size: number;
+  positionX: number;
+  positionY: number;
+  color: string;
 }
+const BallElement = styled.div<IBallElementProps>`
+  width: ${(props) => `${props.size}px`};
+  height: ${(props) => `${props.size}px`};
+  border-radius: 50%;
+  background-color: ${(props) => `${props.color}`};
+  position: absolute;
+  z-index:3 ;
+  top: ${(props) => `${props.positionX}px`};
+  left: ${(props) => `${props.positionY}px`};
+`;
 
-export default Ball
+const Ball = (): JSX.Element => {
+  return <BallElement size={100} positionX={40} positionY={100} color={"green"}></BallElement>;
+};
+
+export default Ball;
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import bg from '../assets/bg-canvas.svg'
 import "../styles/canvas.css"
+import Ball from './Ball'
 import Navigation from './Navigation'
-const Canvas = ():JSX.Element => {
+const Canvas: React.FC = ():JSX.Element => {
   return (
     <main style={{backgroundImage:`url(${bg})`}} className="canvas-container">
         <Navigation/>
+        <Ball/>
     </main>
   )
 }

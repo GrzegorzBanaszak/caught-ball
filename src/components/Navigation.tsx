c892ff3fceb45fe53d7f6d6b50f9552f78bc3490
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-26 16:29:08 +0200

    Fix display seconds in navbar timer

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,7 +10,7 @@ const Navigation: React.FC<INavigation> = ({ roundTime, score }): JSX.Element =>
   return (
     <nav>
       <div className="nav-container">
-        <div className="nav-info">Time {`${Math.floor(roundTime / 60)}:${roundTime % 60}`}</div>
+        <div className="nav-info">Time {`${Math.floor(roundTime / 60)}:${roundTime % 60 < 10 ? "0" + roundTime % 60 : roundTime % 60}`}</div>
         <div className="nav-info">Points {score}</div>
         <div className="nav-info">Points to next level</div>
 

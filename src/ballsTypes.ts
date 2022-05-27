8688399ba6605fdc32937a9d01948eb46b034bbe
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-27 18:01:37 +0200

    Add function for create collection balls with collors

--- src/ballsTypes.ts
+++ src/ballsTypes.ts
@@ -52,3 +52,71 @@ export const ballTypes: IBallType[] = [
     points: 100,
   },
 ];
+
+/**
+ * Create collection of balls
+ * @param colorBall80 Color for ball of size 80px 30pt
+ * @param colorBall60 Color for ball of size 60px 50pt
+ * @param colorBall40 Color for ball of size 40px 70pt
+ * @param colorBall30 Color for ball of size 30px 10pt
+ * @returns Collection of balls
+ */
+export const getBallTypes = (
+  colorBall80: string,
+  colorBall60: string,
+  colorBall40: string,
+  colorBall30: string
+): IBallType[] => {
+  return [
+    {
+      size: 80,
+      color: colorBall80,
+      points: 30,
+    },
+    {
+      size: 80,
+      color: colorBall80,
+      points: 30,
+    },
+    {
+      size: 80,
+      color: colorBall80,
+      points: 30,
+    },
+    {
+      size: 80,
+      color: colorBall80,
+      points: 30,
+    },
+    {
+      size: 60,
+      color: colorBall60,
+      points: 50,
+    },
+    {
+      size: 60,
+      color: colorBall60,
+      points: 50,
+    },
+    {
+      size: 60,
+      color: colorBall60,
+      points: 50,
+    },
+    {
+      size: 40,
+      color: colorBall40,
+      points: 70,
+    },
+    {
+      size: 40,
+      color: colorBall40,
+      points: 70,
+    },
+    {
+      size: 30,
+      color: colorBall30,
+      points: 100,
+    },
+  ];
+};

c46ed59c3c2216fa0b658967ab422f48a098b0e7
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-27 15:22:33 +0200

    Add description to game function

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -30,9 +30,7 @@ const useGame = (): GameReturnType => {
 
   useEffect(() => {
     if (isPlaing) {
-      const timeIntervalId = createTimer(30);
-      // generateRandomBall();
-
+      const timeIntervalId = createTimer(level.gameTime);
       return () => {
         window.clearInterval(timeIntervalId);
       };
@@ -41,10 +39,19 @@ const useGame = (): GameReturnType => {
 
   useEffect(() => {
     if (isPlaing) {
-      if (balls.length < 40 && roundTime > 4) generateRandomBall();
+      if (
+        balls.length < level.ballsLimit &&
+        roundTime > Math.ceil(level.timeOfBallDisplay[1] / 1000)
+      )
+        generateRandomBall();
     }
   }, [roundTime, isPlaing]);
 
+  /**
+   * Create timer when game is start
+   * @param time Initial time in seconds
+   * @returns return interval id
+   */
   const createTimer = (time: number): number => {
     const timeIntervalId = window.setInterval(() => {
       setRoundTime((prev) => {
@@ -62,13 +69,23 @@ const useGame = (): GameReturnType => {
     }, 1000);
     return timeIntervalId;
   };
-
+  /**
+   * Create timer when game is start
+   * @param min minimal range value
+   * @param max maximum range value
+   * @returns random number value form min - max range
+   */
   function getRandomInt(min: number, max: number) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
+  /**
+   * Create random position on screen
+   * @param size Size of ball
+   * @returns position x and y in screen
+   */
   const randomPositonBall = (size: number) => {
     let positionX = getRandomInt(0, window.innerWidth);
     let positionY = getRandomInt(104, window.innerHeight);
@@ -88,8 +105,14 @@ const useGame = (): GameReturnType => {
     return { positionX, positionY };
   };
 
+  /**
+   * Generate random ball from balls type and display on screen
+   */
   const generateRandomBall = (): void => {
-    const timeToShowAndRemove = getRandomInt(1000, 3000);
+    const timeToShowAndRemove = getRandomInt(
+      level.timeOfBallDisplay[0],
+      level.timeOfBallDisplay[1]
+    );
     const { size, color, points } = ballTypes[getRandomInt(0, 9)];
     createBall(size, color, points, timeToShowAndRemove);
     setTimeout(() => {
@@ -100,6 +123,14 @@ const useGame = (): GameReturnType => {
     }, timeToShowAndRemove);
   };
 
+  /**
+   * Create ball and add auto remove
+   * @param size Size of ball
+   * @param color Color ball
+   * @param points Points on click ball
+   * @param time Time to ball remove
+   * @returns New ball
+   */
   const createBall = (
     size: number,
     color: string,
@@ -118,16 +149,25 @@ const useGame = (): GameReturnType => {
       points,
     };
   };
-
+  /**
+   * Remove ball from balls list
+   * @param ballId Id of ball to remove
+   */
   const removeBall = (ballId: string): void => {
     setBalls((prev) => prev.filter((ball) => ball.id !== ballId));
   };
-
+  /**
+   * Remove ball on click and add points
+   * @param ballId Id of ball to remove
+   * @param pointsToAdd Points to add
+   */
   const onBallClick = (ballId: string, pointsToAdd: number): void => {
     if (isPlaing) setScore((prev) => prev + pointsToAdd);
     setBalls((prev) => prev.filter((ball) => ball.id !== ballId));
   };
-
+  /**
+   * Start game on click
+   */
   const onClickStart = (): void => {
     setIsPlaing(true);
     setGameState(GameStateEnum.Plaing);
